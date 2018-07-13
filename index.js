3265ca7827218dbbd91331ffc8566c3b5071d0a2
Oscar Adams <oadams.co@gmail.com>
2018-07-13 18:12:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  return Object.assign({}, delete object, [key])
+  return Object.assign({}, delete object[key])
   
 }
