a79d2c9cab2b18a85c8a2249fe4f9c501925cef6
Oscar Adams <oadams.co@gmail.com>
2018-07-13 17:36:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return object
+  return Object.assign({}, object[key] = value)
+  
+  }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+  
+}
