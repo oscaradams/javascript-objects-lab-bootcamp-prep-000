09f1165a93429565613c7c1f22db8c3b57812f9b
Oscar Adams <oadams.co@gmail.com>
2018-07-13 18:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
   
+  return object
   
 }
